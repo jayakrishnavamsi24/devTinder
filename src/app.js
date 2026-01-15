2dd102f8a78932a34f937115e5b92b489e8e8ead
jayakrishnavamsi24 <jayakrishnavamsi24@gmail.com>
2026-01-15 14:39:10 +0530

    Added API level validation

--- src/app.js
+++ src/app.js
@@ -9,11 +9,69 @@ app.use(express.json())
 app.post("/signup", async (req, res) => {
   const user = new User(req.body)
 
+  const userArray = await User.find({ emailId: req.body.emailId })
+
   try {
+    if (userArray.length !== 0) {
+      res.status(500).send("User Already Exists")
+    }
     await user.save()
     res.send("User Added Successfully")
   } catch (e) {
-    res.send("Unable to add User" + e)
+    res.status(500).send("Unable to add User" + e)
+  }
+})
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId
+
+  try {
+    const user = await User.findOne({ emailId: userEmail })
+    if (user.length == 0) {
+      res.send("User not exists")
+    }
+    res.send(user)
+  } catch (e) {
+    res.send("Something went wrong")
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({})
+
+    res.send(users)
+  } catch (e) {
+    res.status(500).send("Something went wrong")
+  }
+})
+
+app.patch("/user", async (req, res) => {
+  const data = req.body
+  const userId = data.userId
+
+  try {
+    const ALLOWED_UPDATES = ["userId", "photoUrl", "about", "gender", "age"]
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    )
+
+    if (!isUpdateAllowed) {
+      throw new Error("Not Allowed fields exist")
+    }
+
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills cannot be more than 10")
+    }
+
+    await User.findByIdAndUpdate({ _id: userId }, req.body, {
+      runValidators: true,
+    })
+
+    res.send("User Updated Successfully")
+  } catch (e) {
+    res.status(500).send(e.message)
   }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,31 +1,54 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
-        type: String
+      type: String,
+      maxLength: 50,
     },
     emailId: {
-        type: String 
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String
-    }, 
+      type: String,
+      minLength: 8,
+    },
     age: {
-        type: Number 
+      type: Number,
+      min: 18,
     },
     gender: {
-        type: String 
-    }
-})
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid")
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema)
 
-module.exports = User;
-
-
-
-
-
+module.exports = User
